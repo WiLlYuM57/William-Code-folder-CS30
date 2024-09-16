d8055009840ad0f8cdaaaa2515eb1fad0cdd9497
WiLlYuM57 <Schmidtwilliam694@gmail.com>
2024-09-16 08:54:03 -0600

    commit

---
+++ 1-Demos and Examples/Lesson4demo/src/App.java
@@ -0,0 +1,15 @@
+// Project name: Lesson 4 Demo 
+//Name: William Schmidt 
+//Date: 13/sept/2024
+//Description: Going over comments and special characters. 
+/* */
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Wor\\ld!");
+        System.out.println("\thow is yo\nur day going?");
+        System.out.println("Its \"cloudy\" with a chance of meatballs today");
+    }
+}
