a46f4452424e97dbfe6166057efb86896fab4409
WiLlYuM57 <Schmidtwilliam694@gmail.com>
2024-09-19 09:02:59 -0600

    chicken wing

---
+++ A4 ASCII animal/src/ASSSIC.java
@@ -0,0 +1,17 @@
+public class ASSSIC {
+    public static void main(String[] args) throws Exception {
+        System.out.println("                               _________________                                                                      ");
+        System.out.println("                              / ###         #      \\                                                                  ");
+        System.out.println("     _____                   /         ##    #   \\                   _____                                          ");
+        System.out.println("    (=o o=)                 /   ##       #     #  \\                 (=o o=)                                     ");
+        System.out.println("     \\===/                 /               ####    \\                 \\===/                                 ");
+        System.out.println("   /=======\\              /        ##               \\              /=======\\                                   ");
+        System.out.println("  /=/|===|\\=\\            /     ##               #    \\            /=/|===|\\=\\                          ");
+        System.out.println(" /=/ |===| \\=\\          /               ######        \\          /=/ |===| \\=\\                        ");
+        System.out.println("///\\ |===| /\\\\\\        /                       ##      \\        ///\\ |===| /\\\\\\                       ");
+        System.out.println("    /=| |=\\           /         ########                \\           /=| |=\\                                  ");
+        System.out.println("   /==| |==\\         /       #               ###         \\         /==| |==\\                          ");
+        System.out.println("  |===| |===|       /    ########         #     ###   \\           |===| |===|                ");
+        System.out.println(" _|===| |===|_     /_______________________________________\\     _|===| |===|_          ");
+    }
+}
