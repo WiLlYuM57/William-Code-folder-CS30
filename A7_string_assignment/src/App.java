8ebbd222165445d4d781ef589261612db90a1709
WiLlYuM57 <Schmidtwilliam694@gmail.com>
2024-09-20 09:35:34 -0600

    end of 9/20/2024

---
+++ A7_string_assignment/src/App.java
@@ -0,0 +1,20 @@
+//public class assignment 7 
+//William Schmidt 
+//9/20/2024
+public class App {
+    public static void main(String[] args) throws Exception {
+        String message1 = "I am very happy";
+        String target1 = "very";
+
+        String message2 = "that was great - lol.";
+        String target2 = "lol.";
+       
+        int newmsg = message1.indexOf(target1);
+    
+
+        System.out.println(newmsg);
+        System.out.println(newmsg1);
+
+
+    }
+}
