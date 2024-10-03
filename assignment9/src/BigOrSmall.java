5400ebbbd3fc5d0c5c4a0c2940ad51413d23b0e8
WiLlYuM57 <Schmidtwilliam694@gmail.com>
2024-10-03 09:47:33 -0600

    end of 10/3/2024

---
+++ assignment9/src/BigOrSmall.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class BigOrSmall{
+public void check( int a , int b ){
+    if (a>b){
+        System.out.println("yes");
+    }
+    else if (a<b){
+    System.out.println("no");
+    }
+    else {
+    System.out.println("equals");
+    }
+}}
---
+++ assignment9/src/BigOrSmallrunner.java
@@ -0,0 +1,25 @@
+//assignment 9
+//William Schmidt 
+// ocotber 3 2024 
+//compare two inputs. 
+public class BigOrSmallrunner {
+    public static void main(String[] args) throws Exception {
+     BigOrSmall java = new BigOrSmall();
+     java.check(10, 20);
+     java.check(20, 10);
+     java.check(20, 20);
+     java.check(10, 10);
+     java.check(0, 1);
+     java.check(1, 0);
+     java.check(3, 5);
+     java.check(5, 3);
+     java.check(55342, 323
+     );
+
+
+
+
+
+
+    }
+}
