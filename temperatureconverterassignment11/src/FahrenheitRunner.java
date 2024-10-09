d483f31eaf3eaf22b449ba18acfd0802eeca21db
WiLlYuM57 <Schmidtwilliam694@gmail.com>
2024-10-09 09:49:31 -0600

    end of 10/9/2024

--- temperatureconverterassignment11/src/FahrenheitRunner.java
+++ temperatureconverterassignment11/src/FahrenheitRunner.java
@@ -2,12 +2,20 @@
 //Date -
 //Class -
 //Lab  -
-
+import java.util.Scanner;
 public class FahrenheitRunner
 {
-	public static void main( String[] args )
-	{
-		//add test cases
-				
+	public static void main( String[] args ){
+		Scanner keyboard = new Scanner(System.in);
+		Fahrenheit asd = new Fahrenheit();
+
+		System.out.println("what is the temperature in celsius?");
+		double c = keyboard.nextDouble();	
+		asd.fahrencalculator(c);
+
+		System.out.println("what is the temperature in Fahrenheit?");
+		double f = keyboard.nextDouble();	
+		asd.celcalculator(f);
+		
 	}
 }
