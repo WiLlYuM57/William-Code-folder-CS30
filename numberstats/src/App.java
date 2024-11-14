977a75af259b14c1d0b4e2994ac89068b011dcd4
WiLlYuM57 <Schmidtwilliam694@gmail.com>
2024-11-14 09:46:16 -0600

    end of 11/14/2024

---
+++ numberstats/src/App.java
@@ -0,0 +1,11 @@
+//William Schmidt 
+//cs 30 
+//Mr. Fox 
+//run a loop froma givens start number to a given end number. 
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
